dbeba761e43e1e10232095bb6ca109dc5a91abca
Jakob’s MacBook <jakob.petersen6779@stu.mtec.edu>
2023-06-12 08:16:47 -0600

    First commit

---
+++ SQLite-playground/SQLite_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLite_playgroundApp.swift
+//  SQLite-playground
+//
+//  Created by mac on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SQLite_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
